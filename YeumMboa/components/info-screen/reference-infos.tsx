2aecf4fbf91452928487f6508dba8adbdf9961bb
kamdem Brice <kamdembrice770@gmail.com>
2026-02-27 19:29:28 +0100

    fini avec le composant reference-infos.tsx et la page info-screen en générale

--- YeumMboa/components/info-screen/reference-infos.tsx
+++ YeumMboa/components/info-screen/reference-infos.tsx
@@ -1,6 +1,9 @@
 import { View, Text, StyleSheet, Image } from 'react-native';
 import {colors} from '../../theme/color';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 
 export default function ReferenceInfo(){
@@ -26,6 +29,35 @@ export default function ReferenceInfo(){
                     <Text style={styles.text}>
                         Main basse sur le cameroun, Mongo Beti, presence Africaine, Paris, 1972.
                     </Text>
+                </View>
+            </View>
+            <View style={{marginTop:20}}>
+                <Text style={styles.titleinfos}>Informations Technique</Text>
+                <View style={{marginTop:15}}>
+                    <View style={styles.infos}>
+                        <View style={styles.textinfos}>
+                            <Entypo name="info-with-circle" style={styles.iconinfos} />
+                            <Text style={styles.nameinfos}>Version</Text>
+                        </View>
+                        <Text style={styles.nameinfos}>v1.0.0</Text>
+                    </View>
+                    <View style={styles.infos}>
+                        <View style={styles.textinfos}>
+                            <MaterialIcons name="wifi-off" style={styles.iconinfos} />
+                            <Text style={styles.nameinfos}>Mode hors ligne</Text>
+                        </View>
+                        <Text style={styles.horsligneinfos}>Activé</Text>
+                    </View>
+                    <View style={styles.infos}>
+                        <View style={styles.textinfos}>
+                            <SimpleLineIcons name="globe" style={styles.iconinfos} />
+                            <Text style={styles.nameinfos}>Langue disponible</Text>
+                        </View>
+                        <View style={styles.languecontainer}>
+                            <Text style={styles.langue}>FR</Text>
+                            <Text style={styles.langue}>En</Text>
+                        </View>
+                    </View>
                 </View>
             </View>
         </View>
@@ -57,5 +89,47 @@ const styles = StyleSheet.create({
     text:{
         fontFamily:'Montserrat-Regular',
         fontSize:12,
+    },
+    /* infos */
+    titleinfos:{
+        fontFamily:'Montserrat-Bold',
+        fontSize:18,
+    },
+    infos:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        backgroundColor:'white',
+        padding:10,
+        marginBottom:5
+    },
+    textinfos:{
+        flexDirection:'row',
+        gap:5,
+        alignItems:'center',
+    },
+    nameinfos:{
+        fontFamily:'PlusJakartaSans-Bold',
+        fontSize:10,
+    },
+    horsligneinfos:{
+        backgroundColor:colors['s-green'],
+        fontFamily:'Montserrat-Bold',
+        padding:5,
+        fontSize:10,
+        borderRadius:10,
+    },
+    iconinfos:{
+        fontSize:14,
+    },
+    languecontainer:{
+        flexDirection:'row',
+        gap:5
+    },
+    langue:{
+        fontFamily:'Montserrat-Bold',
+        backgroundColor:colors['s-green'],
+        padding:5,
+        fontSize:10
     }
+
 });
